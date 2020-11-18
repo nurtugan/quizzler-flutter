15ff2fcb714822c80364eed6713a74dfb994e6d1
Nurtugan Nuraly <nurtugan757@gmail.com>
2020-11-19 02:05:32 +0600

    Minor fix

--- lib/main.dart
+++ lib/main.dart
@@ -127,7 +127,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                checkAnswer(true);
+                checkAnswer(false);
               },
             ),
           ),
